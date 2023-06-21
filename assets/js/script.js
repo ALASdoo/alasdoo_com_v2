822f2c6e1e7e2c2588914388605a945584970c32
Aleksandar Lakatos <aleksandar@naturesway.co>
2023-06-21 19:56:47 +0200

    Close navigation when user click outside

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,4 +160,19 @@
   //     }
   //   }
   // })
+
+  // Close mobile navigation when user click outside
+  document.addEventListener("DOMContentLoaded", function () {
+    var navigation = document.querySelector(".navbar");
+    var navigationCollapse = document.getElementById("navigation");
+    var navigationButton = document.querySelector(".navbar-toggler");
+    var isMobileNavigation = window.matchMedia("(max-width: 1199px)");
+    
+    document.addEventListener("click", function(event) {
+      if(!navigation.contains(event.target) && isMobileNavigation.matches && navigationCollapse.classList.contains("show")) {
+        navigationButton.click();
+      }
+      
+    })
+  })
 })();
